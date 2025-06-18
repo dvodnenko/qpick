f2995d4f17037df4e06c22ef519fa014fe70827c
dvodnenko <thekovalskii@gmail.com>
2025-06-18 21:06:40 +0300

    chore: connect to postgres, add some config values

--- qpick/qpick/config.py
+++ qpick/qpick/config.py
@@ -5,4 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
+
+## app ##
+
 SECRET_APP_KEY = os.getenv('secret_app_key')
+
+## database ##
+
+DATABASE_NAME = os.getenv('database_name')
+DATABASE_USER = os.getenv('database_user')
+DATABASE_PASSWORD = os.getenv('database_password')
+DATABASE_HOST = os.getenv('database_host')
+DATABASE_PORT = int(os.getenv('database_port'))
