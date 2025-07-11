dd50137fb8a0e84680e58b84defe029523fb931d
dvodnenko <thekovalskii@gmail.com>
2025-07-11 20:09:42 +0300

    feat(admin): add models to admin panel

--- qpick/store/admin.py
+++ qpick/store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Headphone, Cover
+
 # Register your models here.
+
+
+@admin.register(Headphone, Cover)
+class Store(admin.ModelAdmin):
+    ...
