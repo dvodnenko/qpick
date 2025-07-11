0290ea7d0a5387cbbbc7b7bc9fadc1a4d583a8fb
dvodnenko <thekovalskii@gmail.com>
2025-07-11 19:56:00 +0300

    feat(models): add base model for products

--- qpick/store/models.py
+++ qpick/store/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=64, default='')
+    price = models.IntegerField(default=1)
