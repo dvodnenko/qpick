9aabd07f7346a7ca8edb2a2c55f96f1d207c8508
dvodnenko <thekovalskii@gmail.com>
2025-07-13 22:52:16 +0300

    feat: add serializer for headphone & cover

---
+++ qpick/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from qpick.store.models import Headphone, Cover
+
+
+class HeadphoneSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Headphone
+        fields = '__all__'
+
+
+class CoverSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Cover
+        fields = '__all__'
