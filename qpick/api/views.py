283f5455383f12deee73fb3a1ff6903d0fa0320f
dvodnenko <thekovalskii@gmail.com>
2025-07-13 23:02:15 +0300

    feat: add views for headphone & cover

--- qpick/api/views.py
+++ qpick/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from qpick.store.models import Headphone, Cover
+from .serializers import HeadphoneSerializer, CoverSerializer
 
 # Create your views here.
+
+
+class HeadphoneView(viewsets.ModelViewSet):
+    queryset = Headphone.objects.all()
+    serializer_class = HeadphoneSerializer
+
+
+class CoverView(viewsets.ModelViewSet):
+    queryset = Cover.objects.all()
+    serializer_class = CoverSerializer
