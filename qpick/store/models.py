84322f571e564799ed2420a26a84ce7acc3e5672
dvodnenko <thekovalskii@gmail.com>
2025-07-13 20:23:35 +0300

    fix: add default image

--- qpick/store/models.py
+++ qpick/store/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=64, default='')
     price = models.IntegerField(default=1)
+    image = models.ImageField(default='/static/default-image.webp')
 
 
 class Headphone(Product):
