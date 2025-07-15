77e320dcdde2bca401eba9c4a98bdb17bb04e0ce
dvodnenko <thekovalskii@gmail.com>
2025-07-15 17:49:32 +0300

    feat: add header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <div className="left-side">
+                <h2>QPICK</h2>
+            </div>
+                
+            <div className="right-side">
+                <span>❤️</span>
+                <span>🛒</span>
+            </div>
+        </header>
+    )
+}
+
+export default Header
