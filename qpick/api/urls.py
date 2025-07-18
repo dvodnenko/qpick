de2905148df51fc5d942587ded0acd8831f01de8
dvodnenko <thekovalskii@gmail.com>
2025-07-18 21:45:30 +0300

    refactor: add get & post methods in api

--- qpick/api/urls.py
+++ qpick/api/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+
 from .views import HeadphoneView, CoverView
 
-router = DefaultRouter()
-router.register(r'headphone', HeadphoneView)
-router.register(r'cover', CoverView)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('headphones', HeadphoneView.as_view()),
+    path('covers', CoverView.as_view()),
+]
--- qpick/api/views.py
+++ qpick/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from store.models import Headphone, Cover
 from .serializers import HeadphoneSerializer, CoverSerializer
@@ -7,11 +8,31 @@ from .serializers import HeadphoneSerializer, CoverSerializer
 # Create your views here.
 
 
-class HeadphoneView(viewsets.ModelViewSet):
-    queryset = Headphone.objects.all()
-    serializer_class = HeadphoneSerializer
+class HeadphoneView(APIView):
+
+    def get(self, request):
+        queryset = Headphone.objects.all()
+        serializer = HeadphoneSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = HeadphoneSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 
-class CoverView(viewsets.ModelViewSet):
-    queryset = Cover.objects.all()
-    serializer_class = CoverSerializer
+class CoverView(APIView):
+    
+    def get(self, request):
+        queryset = Cover.objects.all()
+        serializer = CoverSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CoverSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
