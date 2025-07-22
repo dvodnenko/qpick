fe1a2049995006c8f39bb7c15ac6a5e4f9fcc38a
dvodnenko <thekovalskii@gmail.com>
2025-07-22 18:31:08 +0300

    feat: add cart item to admin panel
    it will allow admins see all cart items

--- qpick/store/admin.py
+++ qpick/store/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Headphone, Cover
+from .models import Headphone, Cover, CartItem
 
 # Register your models here.
 
 
-@admin.register(Headphone, Cover)
+@admin.register(Headphone, Cover, CartItem)
 class Store(admin.ModelAdmin):
     ...
