debae46eb23ef863d5b41f2c35abbcd31c8ae4b5
dvodnenko <thekovalskii@gmail.com>
2025-07-22 16:57:45 +0300

    feat: make cart & cart_item models

--- qpick/store/models.py
+++ qpick/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from cloudinary_storage.storage import MediaCloudinaryStorage
 
@@ -22,3 +23,14 @@ class Headphone(Product):
 
 class Cover(Product):
     ...
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    session_key = models.CharField(max_length=40, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
