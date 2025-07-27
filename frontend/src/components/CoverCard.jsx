7dc560761f342c01548c401f037dc67c4957903d
dvodnenko <thekovalskii@gmail.com>
2025-07-27 18:24:17 +0300

    feat: change button output approach

--- frontend/src/components/CoverCard.jsx
+++ frontend/src/components/CoverCard.jsx
@@ -1,19 +1,49 @@
-function CoverCard({product}) {
+import { addToCart } from "../services/cart"
 
-    return (
-        <div className="card cover-card" >
-            <div class="cardHeartContainer">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-                </svg>
+
+function CoverCard({product, includeButton = false}) {
+
+    if (includeButton) {
+        return (
+            <div className="card cover-card" >
+                <div class="cardHeartContainer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                    </svg>
+                </div>
+                <img src={product.image} alt="/src/assets/default-image.webp" className="earPodsImage" />
+                <div className="cardInfo">
+                    <div className="cardName">{product.title}</div>
+                    <div className="cardPrice">{product.price}{product.currency}</div>
+                </div>
+                <button onClick={() => {
+                        let quantity = prompt('Enter Quantity: ')
+        
+                        if (quantity <= 0) {
+                            alert('Error: Quantity Must be Greater Than 0')
+                            return null
+                        }
+        
+                        addToCart(product.id, quantity)
+                    }}>Add To Cart</button>
             </div>
-            <img src={product.image} alt="/src/assets/default-image.webp" className="earPodsImage" />
-            <div className="cardInfo">
-                <div className="cardName">{product.title}</div>
-                <div className="cardPrice">{product.price}{product.currency}</div>
+        )
+    } else {
+        return (
+            <div className="card cover-card" >
+                <div class="cardHeartContainer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                    </svg>
+                </div>
+                <img src={product.image} alt="/src/assets/default-image.webp" className="earPodsImage" />
+                <div className="cardInfo">
+                    <div className="cardName">{product.title}</div>
+                    <div className="cardPrice">{product.price}{product.currency}</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default CoverCard
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -65,14 +65,14 @@ function Home() {
                 {headphones.length > 0 ? <h3>Headphones</h3> : <h3>No Headphones Found</h3>}
                 <section>
                     {headphones.map((element, index) => (
-                        <HeadphoneCard product={element} key={`headphone-card-${index}`} />
+                        <HeadphoneCard product={element} key={`headphone-card-${index}`} includeButton={true} />
                     ))}
                 </section>
 
                 {covers.length > 0 ? <h3>Covers</h3> : <h3>No Covers Found</h3>}
                 <section>
                     {covers.map((element, index) => (
-                        <CoverCard product={element} key={`cover-card-${index}`} />
+                        <CoverCard product={element} key={`cover-card-${index}`} includeButton={true} />
                     ))}
                 </section>
             </main>
