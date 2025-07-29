18bc8dea52be063db32d49c0dc29cbe2ebeb888f
dvodnenko <thekovalskii@gmail.com>
2025-07-29 17:25:50 +0300

    feat: add separate model for cart

--- qpick/store/models.py
+++ qpick/store/models.py
@@ -23,12 +23,17 @@ class Headphone(Product):
 class Cover(Product):
     ...
 
-class CartItem(models.Model):
+class Cart(models.Model):
     session_key = models.CharField(max_length=40, blank=True)
+    total_price = models.IntegerField(default=0)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     product_type = models.CharField(max_length=16, default='')
     quantity = models.PositiveIntegerField(default=1)
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.product.title} x{self.quantity} [{self.session_key}]'
+        return f'{self.product.title} x{self.quantity} [{self.cart.session_key}]'
