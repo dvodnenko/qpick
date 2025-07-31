273a71a8ced5ebab488708cbc4ab61ff9324f195
dvodnenko <thekovalskii@gmail.com>
2025-07-31 16:44:21 +0300

    feat(Cart): output cart total price

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -6,17 +6,39 @@ import { orderMyCart } from "../services/order"
 
 
 function Cart({cartItems}) {
-    console.log(cartItems)
+    let totalPriceUSD = 0
+    let totalPriceUAH = 0
+
+    cartItems.map((cartItem, _) => {
+        if (cartItem.product.currency === '$') {
+            totalPriceUSD += cartItem.product.price * cartItem.quantity
+        } else if (cartItem.product.currency === '₴') {
+            totalPriceUAH += cartItem.product.price * cartItem.quantity
+        }
+    })
+
+    let totalPriceString = ''
+    if (totalPriceUAH == 0 && totalPriceUSD == 0)       totalPriceString = '0'
+    else if (totalPriceUSD > 0 && totalPriceUAH == 0)   totalPriceString = `$${totalPriceUSD}`
+    else if (totalPriceUSD == 0 && totalPriceUAH > 0)   totalPriceString = `₴${totalPriceUAH}`
+    else if (totalPriceUAH > 0 && totalPriceUSD > 0)    totalPriceString = `$${totalPriceUSD} + ₴${totalPriceUAH}`
 
     return (
         <>
             <Header titleContent={'My Cart'} />
 
             <main>
-                <div className="buy-button-container">
-                    <button onClick={() => {
-                        orderMyCart()
-                    }}>Order Everything in Cart</button>
+                <div className="cart-info">
+                    <div className="buy-button-container">
+                        <button onClick={() => {
+                            orderMyCart()
+                        }}>Order Everything in Cart</button>
+                    </div>
+
+                    <span style={{
+                        fontWeight: 800,
+                        fontSize: '17px'
+                    }}>Total Price: {totalPriceString}</span>
                 </div>
 
                 {cartItems.length > 0 ? <h3>My Cart Items</h3> : <h3>No Items in Your Cart yet</h3>}
